855b68407c83681236d7d1ed769de58a295d24ee
Sedrickparker1 <sedrick.parker@nextlevelnola.org>
2022-04-26 20:04:57 +0000

    pong

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -21,21 +21,11 @@ function runProgram() {
     item.speedY = 0;
     return item;
   }
-  function MakeBall(elementId) {
-    var item = {};
-    item.id = elementId;
-    item.x = Number($(elementId).css('left').replace(/[^-\d\.]/g, ""));
-    item.y = Number($(elementId).css('top').replace(/[^-\d\.]/g, ""));
-    item.width = $(elementId).width();
-    item.height = $(elementId).height();
-    item.speedX = 2;
-    item.speedY = 2;
-    return item;
-  }
   let pong = MakeGameItem('#player1');
   let pong2 = MakeGameItem('#player2');
-  let ball = MakeBall('#ball');
- 
+  let ball = MakeGameItem('#ball');
+  ball.speedX = 4;
+  ball.speedY = 4; 
   const BOARD_HEIGHT = $('#board').height() - $('#player1').height();
   const boardwidth = $('#board').width() - $('#ball').width();
   const ballBoardHeight = $('#board').height() - $('#ball').height();
@@ -58,7 +48,6 @@ function runProgram() {
   On each "tick" of the timer, a new frame is dynamically drawn using JavaScript
   by calling this function and executing the code inside.
   */
-
   function newFrame() {
     Movement(pong);
     Movement(pong2);
@@ -73,8 +62,8 @@ function runProgram() {
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
     //  calculate the right side of the 1st paddle.
-    let winner = 0;
-    let winner2 = 0;
+  let winner = 0;
+  let winner2 = 0;
   function collide(player1, player2){
       // item sides
       player1.top = player1.y;
@@ -94,10 +83,10 @@ function runProgram() {
       ball.top = ball.y;
   // long ass conditionss
      if(player1.right > ball.left && player1.left < ball.right && player1.top < ball.bottom && player1.bottom > ball.top){
-      ball.speedX += 3;
+      ball.speedX = 4;
      }
      else if(player2.left < ball.right && player2.right > ball.right && player2.top < ball.bottom && player2.bottom > ball.top){
-      ball.speedX -= 2;
+      ball.speedX = -4;
      }
   }   
   function Movement(obj){
@@ -111,50 +100,51 @@ function runProgram() {
     $(ball.id).css("left", ball.x);
     $(ball.id).css('top', ball.y);
     if(ball.x > boardwidth){
-      ball.x = Math.floor((Math.random()* 245) + 250);
-      ball.speedX = Math.floor((Math.random() * -5) + -5);
+      ball.x = Math.floor((Math.random(250)* 260));
+      ball.speedX = Math.floor((Math.random(-3)* -5));
       score('#scoreboard');
       winner++;
      }
+
     else if(ball.x < 0){
-      ball.x = Math.floor((Math.random()* 260) + 265);
-      ball.speedX = Math.floor((Math.random() * 5) + 5); 
+      ball.x = Math.floor((Math.random(250)* 255));
+      ball.speedX = Math.floor((Math.random(3) * 4)); 
       score('#scoreboard2');
       winner2++;
     }
     else if(ball.y > ballBoardHeight){
-      ball.speedY = Math.floor((Math.random() * -6) + -6); ;
+      ball.speedY = Math.floor((Math.random(-3) * -4)); ;
     }
     else if(ball.y < 0){
-      ball.speedY = Math.floor((Math.random() * 6) + 6);
+      ball.speedY = Math.floor((Math.random(3) * 4));
     }
   }
   function handleKeyDown(event){
     if (event.which === KEY.P1UP) {
-      pong.speedY = -7;
-    }
-    else if (event.which === KEY.P1DOWN) {
-      pong.speedY = 7;
-    }
-    if (event.which === KEY.P2UP) {
       pong2.speedY = -7;
     }
-    else if (event.which === KEY.P2DOWN) {
+    else if (event.which === KEY.P1DOWN) {
       pong2.speedY = 7;
+    }
+    if (event.which === KEY.P2UP) {
+      pong.speedY = -7;
+    }
+    else if (event.which === KEY.P2DOWN) {
+      pong.speedY = 7;
     }
   }
   function handleKeyUp(event){
     if (event.which === KEY.P1UP) {
-      pong.speedY = 0;
+      pong2.speedY = 0;
     }
     else if (event.which === KEY.P1DOWN) {
-      pong.speedY = 0;
+      pong2.speedY = 0;
     }
     else if (event.which === KEY.P2UP) {
-      pong2.speedY = 0;
+      pong.speedY = 0;
     }
     else if (event.which === KEY.P2DOWN) {
-      pong2.speedY = 0;
+      pong.speedY = 0;
     }
   }
   function dontLeaveBoard(obj){
@@ -178,9 +168,9 @@ function runProgram() {
       endGame(winner2);
     }
   }
-  // function getScoreFeed(id, newtext ){
-  //   document.getElementById(id).innerHTML = newtext;
-  // }
+  function getScoreFeed(id, newtext ){
+    document.getElementById(id).innerHTML = newtext;
+  }
   function endGame(champ){
     // stop the interval timer
     alert(champ + ' has won this game');
