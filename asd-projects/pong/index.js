f38358ded69011853ee4652fa103c010c0b5c985
Sedrickparker1 <sedrick.parker@nextlevelnola.org>
2022-05-26 21:10:28 +0000

    pong

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -24,9 +24,8 @@ function runProgram() {
   let pong = MakeGameItem('#player1');
   let pong2 = MakeGameItem('#player2');
   let ball = MakeGameItem('#ball');
-  ball.speedX = 4;
-  ball.speedY = 4;
-  let winner , winner2 = 0;
+  let winner  = 0
+  let winner2 = 0;
   const BOARD_HEIGHT = $('#board').height() - $('#player1').height();
   const boardWidth = $('#board').width() - $('#ball').width();
   const ballBoardHeight = $('#board').height() - 9;
@@ -36,10 +35,28 @@ function runProgram() {
     'P2UP': 87,
     'P2DOWN': 83,
   }
-  // one-time setup
+  // one-time setup 
+  startBall();
+
+ function makeItMove(obj){
+  obj.x += obj.speedX;
+  obj.y += obj.speedY;
+  $(obj.id).css('left', obj.x);
+  $(obj.id).css('top', obj.y);
+  getScore(obj);
+  keepBallInBoard();
+ }
+
+ function startBall(){
+   ball.speedX = (Math.random() * 3 + 2) * (Math.random() > 0.5 ? -1 : 1);
+   ball.speedY = (Math.random() * 3 + 2) * (Math.random() > 0.5 ? -1 : 1);
+ }
+
   let interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
   $(document).on('keydown', handleKeyDown);
   $(document).on('keyup', handleKeyUp);
+
+ 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -47,10 +64,11 @@ function runProgram() {
   On each "tick" of the timer, a new frame is dynamically drawn using JavaScript
   by calling this function and executing the code inside.
   */
+  
   function newFrame() {
+    makeItMove(ball);
     movement(pong);
     movement(pong2);
-    makeItMove(ball);
     collide(pong);
     collide(pong2);
     CheckForWin();
@@ -74,25 +92,19 @@ function runProgram() {
     ball.bottom = ball.y + ball.height;
     ball.top = ball.y;
 
-    if (player.right > ball.left && player.left < ball.right && player.top < ball.bottom && player.bottom > ball.top) {
-      ball.speedX = 4;
-    }
-    if (player.left < ball.right && player.right > ball.right && player.top < ball.bottom && player.bottom > ball.top) {
-      ball.speedX = -4;
+    let p2Hit = player.right > ball.right ? -1 : 1;
+
+    if (player.right > ball.left
+       && player.left < ball.right && 
+       player.top < ball.bottom &&
+       player.bottom > ball.top) {
+       ball.speedX = 4 * p2Hit;
     }
   }
   function movement(obj) {
     $(obj.id).css('top', obj.y);
     obj.y += obj.speedY;
     dontLeaveBoard(obj);
-  }
-  function makeItMove(item) {
-    item.x += item.speedX;
-    item.y += item.speedY;
-    $(item.id).css("left", item.x);
-    $(item.id).css('top', item.y);
-    getScore(item);
-    keepBallInBoard(item);
   }
   function handleKeyDown(event) {
     if (event.which === KEY.P1UP) {
@@ -150,14 +162,14 @@ function runProgram() {
       winner2++;
     }
   }
-  function keepBallInBoard(item) {
-    if (item.y > ballBoardHeight) {
-      item.speedY = Math.floor((Math.random() * -4) + -1);
-      item.speedX++;
+  function keepBallInBoard() {
+    if (ball.y > ballBoardHeight) {
+      ball.speedY = Math.floor((Math.random() * -4) + -1);
+      ball.speedX++;
     }
-    else if (item.y < 9) {
-      item.speedY = Math.floor((Math.random() * 4 - 2) + 4);
-      item.speedX++;
+    else if (ball.y < 9) {
+     ball.speedY = Math.floor((Math.random() * 4 - 2) + 4);
+      ball.speedX++;
     }
   }
   // scoreboard for the game ...
@@ -165,16 +177,18 @@ function runProgram() {
     $(id).text((parseFloat($(id).text().replace(/^\D+/g, '')) + 1));
   }
   function CheckForWin(){
-    const gameOver = 7;
-    if (winner === gameOver || winner2 === gameOver){
-        
+    let gameOver = 5;
+    if (winner === gameOver){
+      winner = 'Player 1';
+      endGame(winner);
+    }
+    else if (winner2 === gameOver){
+      winner2 ='Player 2';
+      endGame(winner2);
     }
   }
-    
-    
 
-    
-  
+
   function getScoreFeed(currentScorer) {
     $('#scoreFeed').text('...' + currentScorer + ' has scored !');
   }
